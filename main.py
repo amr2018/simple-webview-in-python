e0399bc0ad9e55da8067a5f0a57fa432942f091d
Free Python Code <71100436+amr2018@users.noreply.github.com>
2024-04-14 14:52:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# pip install tk
+# pip install pywebview
+# url https://www.ysense.com/?rb=151997709
 
+import webview
+
+url = 'https://www.ysense.com/?rb=151997709'
+
+webview.create_window('Test', url=url)
+webview.start()
